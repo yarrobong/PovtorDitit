6473ec8f7e436aced37b2698e0fb99901471bea0
yarrobong <oneboardsholi@gmail.com>
2023-10-26 10:46:09 +0500

    Добавьте файлы проекта.

---
+++ PovtorDitit/Program.cs
@@ -0,0 +1,26 @@
+﻿string str = Console.ReadLine();
+string numberStr = String.Empty;
+string result = String.Empty;
+foreach (char item in str)
+{
+    if(!result.Contains(item))
+    {
+        result = result + item;
+    }    
+}
+foreach (char item in result)
+{
+    int count = 0;
+    foreach (char symbol in str)
+    {
+        if (item == symbol)
+        {
+            count++;
+        }
+    }
+    if (count > 1 && Char.IsDigit(item))
+    {
+        numberStr = numberStr + item;
+    }
+}
+Console.WriteLine(numberStr);
